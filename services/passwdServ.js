a78d4f4b5609a669c57f71cd596ff2f5e8f8f208
lvnam96 <lvnam96@gmail.com>
2018-06-14 15:38:11 +0700

    Switch bcrypt module to support installation on Windows x64

--- services/passwdServ.js
+++ services/passwdServ.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const saltRounds = 10;
 
 // I won't use async/await version of bcrypt
@@ -6,15 +6,17 @@ const saltRounds = 10;
 // makes bcrypt safer when being brute-force attacked
 
 function getHash (rawPasswd, cb) {
-    return bcrypt.hash(rawPasswd, saltRounds)
-        .then(cb)
-        .catch(err => console.error(err));
+    return bcrypt.hash(rawPasswd, saltRounds, (err, hash) => {
+        if (err) return console.error(err);
+        return cb(hash);
+    });
 }
 
 function isRawMatchHashedPasswd (rawPasswd, hashedPasswd, cb) {
-    return bcrypt.compare(rawPasswd, hashedPasswd)
-        .then(cb)
-        .catch(err => console.error(err));
+    return bcrypt.compare(rawPasswd, hashedPasswd, (err, res) => {
+        if (err) return console.error(err);
+        return cb(res);
+    });
 }
 
 module.exports = {
