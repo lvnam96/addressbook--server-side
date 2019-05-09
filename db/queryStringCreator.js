139c1bf546e295418bca8f1fcbb12e25ff9a0d1e
lvnam96 <lvnam96@gmail.com>
2019-05-09 12:08:04 +0700

    Improve: let database create uuid when inserting new contact

--- db/queryStringCreator.js
+++ db/queryStringCreator.js
@@ -7,4 +7,5 @@ module.exports.whereClauseMatchAccAndAdrsbook = (accountId, adrsbookId) => {
     return pgpQueryFormat('WHERE account_id = $1 AND addressbook_id = $2', [accountId, adrsbookId]);
 };
 
-module.exports.getQueryStrToImportContacts = contacts => pgpHelpers.insert(contacts, ['id', 'addressbook_id', 'account_id', 'name', 'labels', 'birth', 'note', 'email', 'website', 'phone', 'color', 'avatar_url'], 'contact');
+// don't use old ID when importing backed up contacts
+module.exports.getQueryStrToImportContacts = contacts => pgpHelpers.insert(contacts, ['addressbook_id', 'account_id', 'name', 'labels', 'birth', 'note', 'email', 'website', 'phone', 'color', 'avatar_url'], 'contact');
