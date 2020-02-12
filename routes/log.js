35b6b4067d3ca0467b49bc00245b58e2af9d0903
lvnam96 <lvnam96@gmail.com>
2020-02-12 20:08:40 +0700

    Fix: CSP-report route didn't send response to client

--- routes/log.js
+++ routes/log.js
@@ -8,7 +8,7 @@ router.post('/csp-report', express.json, (req, res, next) => {
   adbk.dev.debug(`Blocked file: ${req.body['csp-report']['source-file']}`);
   adbk.dev.debug(`Blocked URI: ${req.body['csp-report']['blocked-uri']}`);
   adbk.dev.debug(`Violated directive: ${req.body['csp-report']['violated-directive']}`);
-  res.end();
+  res.status(200).end('Logged');
 });
 
 module.exports = router;
